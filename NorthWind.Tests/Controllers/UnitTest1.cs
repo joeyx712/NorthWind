c66a3edcc1ab9b337043a509d4c742a66575f1d6
joeylin <joeyx712@gmail.com>
2019-06-30 19:56:46 +0800

    Add Put and Delete Product Test

--- NorthWind.Tests/Controllers/UnitTest1.cs
+++ NorthWind.Tests/Controllers/UnitTest1.cs
@@ -13,7 +13,7 @@ namespace NorthWind.Tests.Controllers
     public class ProductsConrollerTest
     {
         [TestMethod]
-        public async Task GetAllProducts_ShouldReturnAllProducts()
+        public async Task A_GetAllProducts_ShouldReturnAllProducts()
         {
             // Arrange
             var controller = new ProductsController();
@@ -28,7 +28,7 @@ namespace NorthWind.Tests.Controllers
         }
 
         [TestMethod]
-        public async Task PostNewProduct_ShouldReturnNewProducts()
+        public async Task B_PostNewProduct_ShouldReturnNewProduct()
         {
             //Arragnge
             var controller = new ProductsController();
@@ -50,6 +50,49 @@ namespace NorthWind.Tests.Controllers
             var result = await controller.PostProducts(NewProduct);
             var result_test = result as CreatedAtRouteNegotiatedContentResult<Products>;
             //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result_test.Content.ProductName, "Test Product");
+        }
+
+        [TestMethod]
+        public async Task C_PutProduct_ShoudlReturnNonContent()
+        {
+            //Arranage
+            var controller = new ProductsController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new System.Web.Http.HttpConfiguration();
+            var NewProduct = new Products()
+            {
+                ProductID = 1,
+                ProductName = "Chai Modified",
+                SupplierID = 1,
+                CategoryID = 1,
+                QuantityPerUnit = "10 boxes x 20 bags",
+                UnitPrice = 18,
+                UnitsInStock = 39,
+                UnitsOnOrder = 0,
+                ReorderLevel = 10,
+                Discontinued = false
+            };
+            //Act
+            var result = await controller.PutProducts(1, NewProduct);
+            var result_test = result as StatusCodeResult;
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result_test.StatusCode, System.Net.HttpStatusCode.NoContent);
+        }
+
+        [TestMethod]
+        public async Task D_DeleteNewProduct_ShouldReturnOK()
+        {
+            //Arrange
+            var controller = new ProductsController();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new System.Web.Http.HttpConfiguration();
+            //Act
+            var result = await controller.DeleteProducts(78);
+            var result_test = result as OkNegotiatedContentResult<Products>;
+            // Assert
             Assert.IsNotNull(result);
             Assert.AreEqual(result_test.Content.ProductName, "Test Product");
         }
